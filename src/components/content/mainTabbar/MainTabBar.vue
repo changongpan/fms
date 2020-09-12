568b9425e84190e2b70c34c7b228832b96eea254
changongpan <185769024@qq.com>
2020-09-13 07:59:54 +0800

    add MainTabBar

---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <tab-bar>
+    <tab-bar-item path="/home">
+<!--      <img slot="item-icon" src="~assets/img/tabbar/home.svg" alt="">-->
+<!--      <img slot="item-icon-active" src="~assets/img/tabbar/home_active.svg" alt="">-->
+      <div slot="item-text" class="tabbar-content">舱位计划</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+  import TabBar from 'components/common/topbar/TabBar'
+  import TabBarItem from 'components/common/topbar/TabBarItem'
+
+  export default {
+    name: "MainTabBar",
+    components: {
+      TabBar,
+      TabBarItem
+    }
+  }
+</script>
+
+<style scoped>
+.tabbar-content {
+  line-height: 49px;
+}
+</style>
