614ab14915ee41389c012ab264b02a3681d51d40
changongpan <185769024@qq.com>
2020-10-09 20:05:54 +0800

    pan

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Login from 'components/content/login/Login'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
@@ -11,62 +13,3 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
-
-import {dataGet} from "network/request";
-import {dataPost} from "network/request";
-
-// dataGet('home/testdata',
-//   {
-//     id: 100,
-//     name: 'wangwu'
-// }).then(res => {
-//   console.log(res);
-// })
-//   .catch(err => {
-//     console.log(err);
-//   })
-//
-// dataPost('home/testdata',{
-//     id:101,
-//     name: {
-//       n1:'nzzn1',
-//       n2:'nzzn2'
-//     }
-// }).then(res => {
-//   console.log(res);
-// })
-//   .catch(err => {
-//     console.log(err);
-//   })
-
-// request({
-//   url:'home/testdata',
-//   params:{
-//     id:100,
-//     name:'wangwu'
-//   }
-// })
-//   .then(res => {
-//     console.log(res);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   })
-//
-// request({
-//   url:'home/testdata',
-//   method:'post',
-//   data: {
-//     id:101,
-//     name: {
-//       n1:'nzzn1',
-//       n2:'nzzn2'
-//     }
-//   }
-// })
-//   .then(res => {
-//     console.log(res);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   })
--- src/views/route/AddFlight.vue
+++ src/views/route/AddFlight.vue
@@ -1,52 +1,94 @@
 <template>
-<div class="add-flight">
-  <br><br>
-  <label for="flightPOL">起运港：<input type="text" id="flightPOL" v-model="newFlight.flightPOL"></label><br>
-  <label for="flightPOD">目的港：<input type="text" id="flightPOD" v-model="newFlight.flightPOD"></label><br>
-  <label for="flightNo">航班号：<input type="text" id="flightNo" v-model="newFlight.flightNo"></label><br>
-  <label for="flightDate">航班日期：<input type="text" id="flightDate" v-model="newFlight.flightDate"></label><br>
-  <label for="flightType">航班型号：<input type="text" id="flightType" v-model="newFlight.flightType"></label><br>
-  <label for="flightAirline">航司：<input type="text" id="flightAirline" v-model="newFlight.flightAirline"></label><br>
-  <label for="flightMaxLoad">最大载量：<input type="text" id="flightMaxLoad" v-model="newFlight.flightMaxLoad"></label><br>
-  <button @click="addFlight">提交</button>
-</div>
+  <div class="add-flight">
+    <div class="block"><span class="demonstration" validate-event="true">起运港：</span>
+      <el-input v-model="newFlight.flightPOL" placeholder="起运港" clearable></el-input>
+    </div>
+    <div class="block"><span class="demonstration">目的港：</span>
+      <el-input v-model="newFlight.flightPOD" placeholder="目的港" clearable></el-input>
+    </div>
+    <div class="block"><span class="demonstration">航班号：</span>
+      <el-input v-model="newFlight.flightNo" placeholder="航班号" clearable></el-input>
+    </div>
+    <div class="block"><span class="demonstration">航班型号：</span>
+      <el-input v-model="newFlight.flightType" placeholder="航班型号" clearable></el-input>
+    </div>
+    <div class="block"><span class="demonstration">航司：</span>
+      <el-input v-model="newFlight.flightAirline" placeholder="航司" clearable></el-input>
+    </div>
+    <div class="block"><span class="demonstration">最大载量：</span>
+      <el-input v-model="newFlight.flightMaxLoad" placeholder="最大载量" clearable></el-input>
+    </div>
+    <div class="block">
+      <span class="demonstration">航班日期：</span>
+      <el-date-picker
+        v-model="originDate"
+        type="date"
+        placeholder="选择日期">
+      </el-date-picker>
+    </div>
+    <div class="flight_input">
+      <el-button type="primary" @click="addFlight">提交</el-button>
+    </div>
+  </div>
 </template>
 
 <script>
   import {dataPost} from "network/request";
 
   export default {
-        name: "AddFlight",
-      data(){
-          return{
-            newFlight:{
-              flightPOL:'',
-              flightPOD:'',
-              flightNo:'',
-              flightDate:'',
-              flightType:'',
-              flightAirline:'',
-              flightMaxLoad:''
-            }
+    name: "AddFlight",
+    data() {
+      return {
+        newFlight: {
+          flightPOL: '',
+          flightPOD: '',
+          flightNo: '',
+          flightDate: '',
+          flightType: '',
+          flightAirline: '',
+          flightMaxLoad: ''
+        },
+        originDate: ''
+      }
+    },
+    methods: {
+      addFlight() {
+        this.newFlight.flightDate = Date.parse(this.originDate) / 1000;
+        console.log(this.newFlight.flightDate);
+        var time = new Date(this.newFlight.flightDate * 1000);
+        var year = time.getFullYear();
+        var month = time.getMonth() + 1;
+        var date = time.getDate();
+        var hours = time.getHours();
+        var minutes = time.getMinutes();
+        var seconds = time.getSeconds();
+        console.log(year + '-' + add0(month) + '-' + add0(date) + ' ' + add0(hours) + ':' + add0(minutes) + ':' + add0(seconds));
+        dataPost(
+          'routemanage/addflight',
+          this.newFlight
+        ).then(res => {
+          if (res.data == 'success') {
+            console.log('added');
+          } else {
           }
-      },
-      methods:{
-          addFlight(){
-            dataPost(
-              'routemanage/addflight',
-              this.newFlight
-            ).then(res => {
-              if (res.data == 'success') {
-                console.log('added');
-              } else {
-              }
-            }).catch(err => {
-            })
-          }
+        }).catch(err => {
+        })
       }
     }
+  }
+
+  function add0(m) {
+    return m < 10 ? '0' + m : m
+  }
 </script>
 
 <style scoped>
-
+  .el-input{
+    width: 400px;
+  }
+  .demonstration{
+    display: inline-block;
+    text-align: right;
+    width: 400px;
+  }
 </style>
